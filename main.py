187371ab63e2eecb0e8bd153c639cfcd4ea46ebe
drlummis <194468973+drlummis@users.noreply.github.com>
2025-01-17 05:05:19 -0500

    Prints a report

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 def main():
-    book = "books/frankenstein.txt"
-    text = get_file_text(book)
+    book_path = "books/frankenstein.txt"
+    text = get_file_text(book_path)
     word_count = get_word_count(text)
-    chars = get_character_counts(text)
-    print(word_count)
-    print(chars)
+    chars_dict = get_character_counts(text)
+    print_report(book_path, word_count, chars_dict)
 
 def get_file_text(path):
     with open(path) as f:
@@ -17,11 +16,41 @@ def get_word_count(text):
 
 def get_character_counts(text):
     chars = {}
-    for char in text.lower():
-        if char in chars:
-            chars[char] += 1
-        else:
-            chars[char] = 1
+    for char in text:
+        if char.isalpha():
+            char = char.lower()
+            if char in chars:
+                chars[char] += 1
+            else:
+                chars[char] = 1
     return chars
+
+def print_report(book_path, word_count, chars_dict):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+    print_character_counts(chars_dict)
+    print("--- End report ---")
+
+def print_character_counts(chars_dict):
+    chars_list = convert_chars_dict_to_list(chars_dict)
+    sort_chars_list(chars_list)
+    for dic in chars_list:
+        char = dic["letter"]
+        count = dic["count"]
+        print(f"The '{char}' character was found {count} times")
+
+def convert_chars_dict_to_list(chars_dict):
+    chars_list = []
+    for key in chars_dict:
+        count = chars_dict[key]
+        chars_list.append({ "letter" : key, "count" : count})
+    return chars_list
+
+def sort_chars_list(chars_list):
+    chars_list.sort(reverse=True, key=sort_chars_list_key)
+
+def sort_chars_list_key(chars_dict):
+    return chars_dict["count"]
 
 main()
