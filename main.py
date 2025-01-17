6cbade33409f9f9da80fec59917bf4dcb7d3e6d8
drlummis <194468973+drlummis@users.noreply.github.com>
2025-01-17 03:14:03 -0500

    Counts character frequency in book

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book = "books/frankenstein.txt"
     text = get_file_text(book)
     word_count = get_word_count(text)
+    chars = get_character_counts(text)
     print(word_count)
+    print(chars)
 
 def get_file_text(path):
     with open(path) as f:
@@ -12,5 +14,14 @@ def get_file_text(path):
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+def get_character_counts(text):
+    chars = {}
+    for char in text.lower():
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
 
 main()
